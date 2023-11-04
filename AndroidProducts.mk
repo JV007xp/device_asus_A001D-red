fa23c96be8dc99e32f61ef90d3f1549a786883c3
JV007xp <114124246+JV007xp@users.noreply.github.com>
2023-11-03 22:45:48 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_A001D.mk
+    $(LOCAL_DIR)/derp_A001D.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_A001D-user \
-    lineage_A001D-userdebug \
-    lineage_A001D-eng
+    derp_A001D-user \
+    derp_A001D-userdebug \
+    derp_A001D-eng
